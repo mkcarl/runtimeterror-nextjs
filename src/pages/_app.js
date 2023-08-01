5536d7fdd35a53f36ca39af853a3922d086b8c81
mkcarl <munkye29@gmail.com>
2023-08-01 14:02:29 +0800

    removed global styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import {AuthProvider} from "@/contexts/AuthContext";
+import {CssBaseline} from "@mui/material";
 
 export default function App({Component, pageProps}) {
     return <AuthProvider>
+        <CssBaseline/>
         <Component {...pageProps} />
 
     </AuthProvider>
